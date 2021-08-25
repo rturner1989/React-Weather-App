d81c91328c2121cb864e714b133cf5c154a83f8f
rturner1989 <robturner1000@hotmail.co.uk>
2021-08-25 21:22:05 +0100

    CSS Update
    - changed layout of forecast cards
    - added basic styling to forecast cards

--- src/components/DailyForecastCard.js
+++ src/components/DailyForecastCard.js
@@ -10,10 +10,38 @@ const DailyForecastCard = ({ day }) => {
                 src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
                 alt="weather icon"
             />
-            <p className="weather-info">Morning {day.temp.morn}&deg;C</p>
-            <p className="weather-info">Afternoon {day.temp.day}&deg;C</p>
-            <p className="weather-info">Evening {day.temp.eve}&deg;C</p>
-            <p className="weather-info">Night {day.temp.night}&deg;C</p>
+            <p className="forecast-info">{day.weather[0].description}</p>
+            <table>
+                <tr>
+                    <td></td>
+                    <td>Temp</td>
+                    <td>Feels Like</td>
+                </tr>
+                <tr>
+                    <td>Morning</td>
+                    <td>{day.temp.morn}&deg;C</td>
+                    <td>{day.feels_like.morn}&deg;C</td>
+                </tr>
+                <tr>
+                    <td>Afternoon</td>
+                    <td>{day.temp.day}&deg;C</td>
+                    <td>{day.feels_like.day}&deg;C</td>
+                </tr>
+                <tr>
+                    <td>Evening</td>
+                    <td>{day.temp.eve}&deg;C</td>
+                    <td>{day.feels_like.eve}&deg;C</td>
+                </tr>
+                <tr>
+                    <td>Night</td>
+                    <td>{day.temp.night}&deg;C</td>
+                    <td>{day.feels_like.night}&deg;C</td>
+                </tr>
+            </table>
+            {/* <p className="forecast-info">Morning {day.temp.morn}&deg;C</p>
+            <p className="forecast-info">Afternoon {day.temp.day}&deg;C</p>
+            <p className="forecast-info">Evening {day.temp.eve}&deg;C</p>
+            <p className="forecast-info">Night {day.temp.night}&deg;C</p> */}
         </div>
     );
 };
