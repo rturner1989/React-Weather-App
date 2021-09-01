bea3f8137e521a05c5e32606c85b152d20ee1df7
rturner1989 <robturner1000@hotmail.co.uk>
2021-09-01 21:05:09 +0100

    CSS Update

--- src/components/SearchMenu.js
+++ src/components/SearchMenu.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { useGlobalContext } from "../context";
 import { GrLocation } from "react-icons/gr";
 
